0d0b42cb6b0f99478f4ee8af11221504b763dbe1
dppangelajhs <evanwangsa1122@gmail.com>
2024-10-27 08:52:55 +0700

    Update launch-countdown.js

--- js/launch-countdown.js
+++ js/launch-countdown.js
@@ -1,4 +1,4 @@
-const launchDate = new Date("2024-10-29T15:00:00").getTime();
+let launchDate = new Date("2024-10-29T15:00:00").getTime();
 let prevDays, prevHours, prevMinutes, prevSeconds;
 let countdownInterval; 
 
